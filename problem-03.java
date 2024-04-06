71f6f5ee5f5cd115a59e496c4448672936712e6d
vishalsavarna <154412107+vishalsavarna@users.noreply.github.com>
2024-04-06 07:26:19 +0530

    Update problem-03.java

---
+++ problem-03.java
@@ -0,0 +1,43 @@
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        //soln without stack
+        //somewhat inspired from Algo Made Easy
+        
+        char ch[] = s.toCharArray(); 
+        //since, we can't mark in string(immutable)
+        //so, we converted string to charArray to make some changes in the array(Basically to mark)
+
+
+        //first pass(left->right), identifying excess closing parenthesis and marking them.
+        int count=0;
+        for(int i=0;i<ch.length;i++){
+            if(ch[i]=='(') count++;
+            else if(ch[i]==')'){
+                if(count>0) count--;
+                else ch[i]='#';
+            }
+        }
+
+        //2nd pass (right->left), identifying excess open parenthesis and marking them(same logic).
+        count=0;
+        for(int i=ch.length-1;i>=0;i--){
+            if(ch[i]==')') count++;
+            else if(ch[i]=='('){
+                if(count>0) count--;
+                else ch[i]='#';
+            }
+        }
+
+        //introduced the stringBuilder class(would have done using string also)
+        StringBuilder res = new StringBuilder();
+
+        for(int i=0;i<ch.length;i++){
+            if(ch[i]!='#') res.append(ch[i]);
+        }
+
+        return res.toString();
+
+
+
+    }
+}
