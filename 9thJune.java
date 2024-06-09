62961cfdf7817534ed59044c617b006b1ab074f8
vishalsavarna <154412107+vishalsavarna@users.noreply.github.com>
2024-06-09 15:48:40 +0530

    974. Subarray Sums Divisible by K

---
+++ 9thJune.java
@@ -0,0 +1,25 @@
+//974. Subarray Sums Divisible by K
+
+class Solution {
+    public int subarraysDivByK(int[] nums, int k) {
+        //Taken help & code from LEditorial
+        //Optimal soln
+        int n = nums.length;
+        int prefixMod = 0, result = 0;
+
+        // There are k mod groups 0...k-1.
+        int[] modGroups = new int[k];
+        modGroups[0] = 1;
+
+        for (int num: nums) {
+            // Take modulo twice to avoid negative remainders.
+            prefixMod = (prefixMod + num % k + k) % k;
+            // Add the count of subarrays that have the same remainder as the current
+            // one to cancel out the remainders.
+            result += modGroups[prefixMod];
+            modGroups[prefixMod]++;
+        }
+
+        return result;
+    }
+}
